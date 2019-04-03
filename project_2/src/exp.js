e4cebbc785ec0b0c7ee0079e9b59194c67316bfd
MuhammadHakami <mmm.mephiles@gmail.com>
2019-04-03 11:33:48 +0300

    finished skills section without links

--- project_2/src/exp.js
+++ project_2/src/exp.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
-
 export default class exp extends Component {
   render() {
     return (
-    <div className="project mt-5">
+    <div className="project mt-5 each-card">
         <div className="col-sm">
-            <img src="https://placehold.it/300x300" className="card-img-top card-img" alt="..."/>
+        <h6 className="card-text mb-1">{this.props.expTitle}</h6>
+            <a href={this.props.expUrl} target="_blank"><img src={process.env.PUBLIC_URL+this.props.expImage} className="card-img" alt="..."/></a>
             <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <ul>
+                    <li>
+                        {this.props.expDesc1}
+                    </li>
+                    <li>
+                        {this.props.expDesc2}
+                    </li>
+                </ul>
             </div>
         </div>
+        <hr/>
     </div>
     )
   }
--- project_2/src/skills.js
+++ project_2/src/skills.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 
 export default class skills extends Component {
   render() {
+      const pro=this.props.skillsPro.map((item,index)=>{
+          return <img className='col-md-3 skill-img col-sm-6 mt-1' src={item} key={index}/>
+      })
+      const fam=this.props.skillsFam.map((item,index)=>{
+        return <img className='col-md-3 skill-img col-sm-6 mt-1' src={item}/>
+    })
     return (
       <div>
-        
+        <h6 className='mt-5 center-text'>Proficient</h6>
+        {pro}
+        <h6 className='mt-5'>Familiar</h6>
+        {fam}
       </div>
     )
   }
